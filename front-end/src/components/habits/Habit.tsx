3cfa8fb1037458170edef77ad202f4a67319d75e
Daniel Chang <daniel.changk@gmail.com>
2022-05-05 22:56:58 -0500

    build my habits page UI and added footer

--- front-end/src/components/habits/Habit.tsx
+++ front-end/src/components/habits/Habit.tsx
@@ -1,7 +1,52 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import { deepOrange } from "@mui/material/colors";
 
-const Habit: React.FC = (props) => {
-  return <div></div>;
+import { getInitials } from "../../util/helpers";
+
+interface HabitItem {
+  id: string;
+  dateStarted: string;
+  partner: string;
+  description: string;
+  count: number;
+}
+
+interface Props {
+  habit: HabitItem;
+  user: { id: string; initials: string };
+}
+
+const Habit: React.FC<Props> = (props) => {
+  let navigate = useNavigate();
+
+  return (
+    <ListItemButton
+      key={props.habit.id}
+      divider
+      onClick={() => navigate(`/${props.user.id}/habits/${props.habit.id}`)}
+    >
+      <ListItemAvatar>
+        <Avatar sx={{ bgcolor: deepOrange[500] }} variant="rounded">
+          {props.user.initials}
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText
+        primary={props.habit.description}
+        secondary={`Last entry: ${props.habit.dateStarted}`}
+        sx={{ textAlign: "center" }}
+      />
+      <ListItemAvatar>
+        <Avatar sx={{ bgcolor: deepOrange[500] }} variant="rounded">
+          {getInitials(props.habit.partner)}
+        </Avatar>
+      </ListItemAvatar>
+    </ListItemButton>
+  );
 };
 
 export default Habit;
--- front-end/src/components/habits/HabitsList.tsx
+++ front-end/src/components/habits/HabitsList.tsx
@@ -1,36 +1,51 @@
 import React from "react";
+import List from "@mui/material/List";
+
+import Habit from "./Habit";
+import { getInitials } from "../../util/helpers";
 
 interface Habit {
   id: string;
   dateStarted: string;
+  partner: string;
   description: string;
   count: number;
 }
 
+const DUMMY_USER = { id: "u1", name: "Daniel Chang" };
 const DUMMY_HABITS: Habit[] = [
   {
     id: "h1",
-    dateStarted: new Date().toISOString(),
+    dateStarted: new Date().toLocaleDateString("en-US"),
+    partner: "Lisa Ann",
     description: "Running",
     count: 1,
   },
   {
     id: "h2",
-    dateStarted: new Date().toISOString(),
+    dateStarted: new Date().toLocaleDateString("en-US"),
+    partner: "Eva Notty",
     description: "Reading",
     count: 3,
   },
 ];
 
 const HabitsList: React.FC = (props) => {
+  const userInitials = getInitials(DUMMY_USER.name);
+  const user = { id: DUMMY_USER.id, initials: userInitials };
+  const habitItems = DUMMY_HABITS.map((h) => <Habit user={user} habit={h} />);
+
   return (
-    <div>
-      <ul>
-        {DUMMY_HABITS.map((habit) => (
-          <p>{habit.description}</p>
-        ))}
-      </ul>
-    </div>
+    <List
+      sx={{
+        width: "100%",
+        bgcolor: "background.paper",
+        maxHeight: "50%",
+        overflow: "auto",
+      }}
+    >
+      {habitItems}
+    </List>
   );
 };
 
